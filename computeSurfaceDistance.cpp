d2b53df9848448c0c745cd3a67343db781ee6930
Josh Wolper <joshwolper@gmail.com>
2023-03-08 17:17:49 -0500

    pushing eigen header lib and begin of main file

---
+++ computeSurfaceDistance.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <stdlib.h>
+#include "eigen/Eigen/Dense"
+
+using namespace std;
+
+Eigen::Vector3d v(1,2,3);
+
+//vector<int> PointToGridIndeces(Eigen::Vector3d p);
+
+main(int argc, char* argv[]){
+    cout << v << endl;
+    return 0;
+}
